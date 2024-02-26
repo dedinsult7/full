22d710c6a3586823f84c11bd699192aa8c711229
dedinsult7 <utkirovsuhrob28@gmail.com>
2024-02-26 23:12:12 +0500

    full

---
+++ data.js
@@ -0,0 +1,10 @@
+
+let b=prompt(" string ga tegishli malumot")
+let c=+prompt("info ciritingg")
+
+let a=( Boolean( prompt( "Boolean typedagi malumot kiriting") ) )
+alert(`siz kiritgan ${b} sozi ${typeof (a)} turiga kiradi
+siz kiritgan ${c} sozi ${typeof (c)} turiga kiradi
+siz kirritgan ${a} sozi ${typeof (c)} turiga kiradi`)
+
+
